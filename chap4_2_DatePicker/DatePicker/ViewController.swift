a0a2861071a01737d005e625a86e6e897e048bcd
jhy0409 <inooph@naver.com>
2021-10-22 13:27:00 +0900

    4 - 데이트피커 사용 날짜 선택하기

--- chap4_2_DatePicker/DatePicker/ViewController.swift
+++ chap4_2_DatePicker/DatePicker/ViewController.swift
@@ -8,44 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var lblCurrentTime: UILabel!
-    @IBOutlet weak var lblPickerTime: UILabel!
-    
     let timeSelector: Selector = #selector(ViewController.updateTime)
     let interval = 1.0
     var count = 0
+    var alarmTime: String?
+    
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblPickerTime: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // 1초마다, 해당뷰를, 함수실행(timeSelector, 사용자정보, 반복
         Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
-    
-    
 
     @IBAction func changeDatePicker(_ sender: UIDatePicker) {
         let datePickerView = sender
-        
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd EEE HH:mm:ss" // 년-월-일 요일 시:분:초
+        formatter.dateFormat = "hh:mm aaa"
         formatter.locale = Locale(identifier: "ko_KR")
         lblPickerTime.text = "선택시간 : \(formatter.string(from: datePickerView.date))"
+        
+        alarmTime = formatter.string(from: datePickerView.date)
     }
-    
-    @objc func updateTime() { // 1초마다 변수값을 1증가 -> 라벨 텍스트 내용으로 할당
-//        lblCurrentTime.text = String(count)
-//        count = count + 1
-        
+    @objc func updateTime() {
         let date = NSDate()
-        
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd EEE HH:mm:ss"
+        formatter.dateFormat = "hh:mm aaa"
         formatter.locale = Locale(identifier: "ko_KR")
         lblCurrentTime.text = "현재시간 : \(formatter.string(from: date as Date))"
+        
+        let currentTime = formatter.string(from: date as Date)
+        if(alarmTime == currentTime) {
+            view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = UIColor.white
+        }
     }
-    
+
 }
 
