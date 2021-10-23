e875ac3b6ed4de7569bd5888f3c499deebb3abdc
jhy0409 <inooph@naver.com>
2021-10-23 14:15:51 +0900

    7 - 웹 뷰로 간단한 웹 브라우저 만들기

--- chap7_2_Web/Web/ViewController.swift
+++ chap7_2_Web/Web/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import WebKit
+class ViewController: UIViewController, WKNavigationDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var myWebView: WKWebView!
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let filePath = Bundle.main.path(forResource: "htmlView", ofType: "html")
+        let myUrl = URL(fileURLWithPath: filePath!)
+        let myRequest = URLRequest(url: myUrl)
+        myWebView.load(myRequest)
     }
-
-
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        myActivityIndicator.startAnimating()
+        myActivityIndicator.isHidden = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        myActivityIndicator.startAnimating()
+        myActivityIndicator.isHidden = false
+    }
 }
 
