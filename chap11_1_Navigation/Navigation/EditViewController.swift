f1ee46bfd5311c89138609bd37ff60fcb482a103
jhy0409 <inooph@naver.com>
2021-10-24 09:02:05 +0900

    chap 11-1

--- chap11_1_Navigation/Navigation/EditViewController.swift
+++ chap11_1_Navigation/Navigation/EditViewController.swift
@@ -7,24 +7,45 @@
 
 import UIKit
 
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool)
+}
+
 class EditViewController: UIViewController {
     var textWayValue: String = ""
     var textMessage: String = ""
+    var isOn = false
+    var delegate: EditDelegate?
     
     @IBOutlet weak var txtMessage: UITextField!
     @IBOutlet weak var lblWay: UILabel!
+    @IBOutlet weak var swIsOn: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+
         lblWay.text = textWayValue
         txtMessage.text = textMessage
+        swIsOn.isOn = isOn
     }
     
-    @IBAction func btnDone(_ sender: UIButton) {
-        navigationController?.popViewController(animated: true)
+    @IBAction func btnDone(_ sender: UIButton) { 
+        if delegate != nil { // 뷰컨 prepare에서 할당되어 들어옴, 에딧뷰컨 & 텍스트필드 내용 재할당
+            delegate?.didMessageEditDone(self, message: txtMessage.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
+        }
+        navigationController?.popViewController(animated: true) // 메인화면으로 이동
     }
     
+    @IBAction func swImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            isOn = true
+        } else {
+            isOn = false
+        } 
+    }
     
     /*
     // MARK: - Navigation
@@ -34,6 +55,5 @@ class EditViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
-
+    */ 
 }
--- chap11_1_Navigation/Navigation/ViewController.swift
+++ chap11_1_Navigation/Navigation/ViewController.swift
@@ -7,14 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, EditDelegate {
+    let imgOn = UIImage(named: "lamp_on.png")
+    let imgOff = UIImage(named: "lamp_off.png")
+    
+    var isOn = true
+    
     @IBOutlet weak var txtMessage: UITextField!
+    @IBOutlet weak var imgView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imgView.image = imgOn
     }
-
+    
+    // MARK: - 세그웨이 이용해 화면 전환
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let editViewController = segue.destination as! EditViewController
         if segue.identifier == "editButton" { // 버튼 클릭
@@ -22,6 +30,15 @@ class ViewController: UIViewController {
         } else if segue.identifier == "editBarButton" { // 바 버튼 클릭
             editViewController.textWayValue = "segue : use Bar button"
         }
+        // MARK: - 수정화면으로 텍스트메시지, 전구상태 전달
+        editViewController.textMessage = txtMessage.text!
+        editViewController.isOn = isOn
+        editViewController.delegate = self
+    }
+    
+    // MARK: - 메시지 값을 텍스트 필드에 표시
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txtMessage.text = message // 뷰컨 텍스트필드에 할당
     }
     
     @IBAction func btnGoEdit(_ sender: UIButton) {
@@ -29,6 +46,16 @@ class ViewController: UIViewController {
         self.navigationController?.pushViewController(editViewController, animated: true)
         editViewController.textWayValue = "use Storyboard ID"
     }
-
+    
+    // MARK: - 전구 이미지 값 세팅
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool) {
+        if isOn {
+            imgView.image = imgOn
+            self.isOn = true
+        } else {
+            imgView.image = imgOff
+            self.isOn = false
+        }
+    }
 }
 
