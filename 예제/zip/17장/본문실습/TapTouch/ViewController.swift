dea01fd1bdc4caff60c8ddc3ab4e259a7577747c
jhy0409 <inooph@naver.com>
2021-10-25 16:25:09 +0900

    chap17-2 start

--- 예제/zip/17장/본문실습/TapTouch/ViewController.swift
+++ 예제/zip/17장/본문실습/TapTouch/ViewController.swift
@@ -17,25 +17,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    // MARK: - [ㅇ] 터치시작
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first! as UITouch // 현재 발생한 터치이벤트 가져옴
+        let touch = touches.first! as UITouch
         
         txtMessage.text = "Touches Began"
         txtTapCount.text = String(touch.tapCount)
         txtTouchCount.text = String(touches.count)
     }
     
-    // MARK: - [ㅇ] 터치된 손가락 움직였을 때
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first! as UITouch
         
         txtMessage.text = "Touches Moved"
-        txtTapCount.text = String(touch.tapCount) // touches 세트 안에 포함된 터치 개수 출력 -> 연속으로 몇번 탭?
-        txtTouchCount.text = String(touches.count) // 터치 객체 중 첫번째 객체에서 탭의 갯수를 가져옴 -> 몇개의 손가락 터치했는지?
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
     }
     
-    // MARK: - [ㅇ] 화면에서 손가락 뗐을 때
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first! as UITouch
         
