0984d8d3c6929bad1aa9ce731bcc7f0872283b8f
jhy0409 <inooph@naver.com>
2021-10-25 12:24:45 +0900

    chap 15-1 ~ing

--- 예제/zip/15장/본문실습/CameraPhotoLibrary/ViewController.swift
+++ 예제/zip/15장/본문실습/CameraPhotoLibrary/ViewController.swift
@@ -2,52 +2,43 @@
 //  ViewController.swift
 //  CameraPhotoLibrary
 //
-//  Created by inooph on 2021/10/25.
+//  Created by Ho-Jeong Song on 2020/11/17.
 //
 
 import UIKit
 import MobileCoreServices
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @IBOutlet var imgView: UIImageView!
+
     let imagePicker: UIImagePickerController! = UIImagePickerController()
-    var captureImage: UIImage! // 촬영및 포토라이브러리에서 불러온 사진 저장할 변수
-    var videoURL: URL! // 녹화한 비디오의 URL을 저장할 변수
-    var flagImageSave = false // 이미지 저장 여부를 나타낼 변수
-    
-    
-    @IBOutlet weak var imgView: UIImageView!
+    var captureImage: UIImage!
+    var videoURL: URL!
+    var flagImageSave = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    func myAlert(_ title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
-        alert.addAction(action)
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    // MARK: - [ㅇ] 사진촬영
-    @IBAction func btnCaptureImgFromCamera(_ sender: UIButton) {
-        if(UIImagePickerController.isSourceTypeAvailable(.camera)) {
+    @IBAction func btnCaptureImageFromCamera(_ sender: UIButton) {
+        if (UIImagePickerController.isSourceTypeAvailable(.camera)) {
             flagImageSave = true
             
             imagePicker.delegate = self
             imagePicker.sourceType = .camera
-            imagePicker.mediaTypes =  [kUTTypeImage as String]
+            imagePicker.mediaTypes = [kUTTypeImage as String]
             imagePicker.allowsEditing = false
             
-            // 뷰에 imagePicker가 보이게 함
             present(imagePicker, animated: true, completion: nil)
-        } else { // 카메라 사용할 수 없을 때 경고창
-            myAlert("Camera inaccessable", message: "Application cannot access the camera")
+        }
+        else {
+            myAlert("Camera inaccessable", message: "Application cannot access the camera.")
         }
     }
     
-    // MARK: - [ㅇ] 사진 불러오기
-    @IBAction func btnLoadImgFromLibrary(_ sender: UIButton) {
-        if(UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
+    @IBAction func btnLoadImageFromLibrary(_ sender: UIButton) {
+        if (UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
             flagImageSave = false
             
             imagePicker.delegate = self
@@ -56,12 +47,12 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             imagePicker.allowsEditing = true
             
             present(imagePicker, animated: true, completion: nil)
-        } else {
+        }
+        else {
             myAlert("Photo album inaccessable", message: "Application cannot access the photo album.")
         }
     }
     
-    // MARK: - [ㅇ] 비디오 촬영
     @IBAction func btnRecordVideoFromCamera(_ sender: UIButton) {
         if (UIImagePickerController.isSourceTypeAvailable(.camera)) {
             flagImageSave = true
@@ -72,12 +63,12 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             imagePicker.allowsEditing = false
             
             present(imagePicker, animated: true, completion: nil)
-        } else {
+        }
+        else {
             myAlert("Camera inaccessable", message: "Application cannot access the camera.")
         }
     }
     
-    // MARK: - [ㅇ] 비디오 불러오기
     @IBAction func btnLoadVideoFromLibrary(_ sender: UIButton) {
         if (UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
             flagImageSave = false
@@ -88,36 +79,44 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             imagePicker.allowsEditing = false
             
             present(imagePicker, animated: true, completion: nil)
-        } else {
+        }
+        else {
             myAlert("Photo album inaccessable", message: "Application cannot access the photo album.")
         }
     }
-    
-    // MARK: - [ㅇ] 촬영 및 선택 끝났을 때 호출
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! NSString
-        
+
         if mediaType.isEqual(to: kUTTypeImage as NSString as String) {
-            captureImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-            
+            captureImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+
             if flagImageSave {
                 UIImageWriteToSavedPhotosAlbum(captureImage, self, nil, nil)
             }
-            
+
             imgView.image = captureImage
-        } else if mediaType.isEqual(to: kUTTypeMovie as NSString as String) {
+        }
+        else if mediaType.isEqual(to: kUTTypeMovie as NSString as String) {
             if flagImageSave {
                 videoURL = (info[UIImagePickerController.InfoKey.mediaURL] as! URL)
-                
+
                 UISaveVideoAtPathToSavedPhotosAlbum(videoURL.relativePath, self, nil, nil)
             }
         }
+
         self.dismiss(animated: true, completion: nil)
     }
-    
-    // MARK: - [ㅇ] 사진 또는 비디오 미촬영 & 미선택시 호출
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.dismiss(animated: true, completion: nil)
+    }
+
+    func myAlert(_ title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let action = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
