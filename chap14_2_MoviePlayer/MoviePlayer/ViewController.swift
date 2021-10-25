9710bd5533e4f91d66d80ccb5bc037daeeee0b68
jhy0409 <inooph@naver.com>
2021-10-25 10:39:14 +0900

    14 - 멀티미디어 활용하기

--- chap14_2_MoviePlayer/MoviePlayer/ViewController.swift
+++ chap14_2_MoviePlayer/MoviePlayer/ViewController.swift
@@ -24,12 +24,27 @@ class ViewController: UIViewController {
         playVideo(url: url)
     }
     
+    @IBAction func btnPlayInternalMovie2(_ sender: UIButton) {
+        let filePath: String? = Bundle.main.path(forResource: "Mountaineering", ofType: "mov")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+    }
+    
+    
     // MARK: - [ㅇ] 외부 mp4 파일
     @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
         let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
         
         playVideo(url: url)
     }
+    
+    @IBAction func btnPlayExternalMovie2(_ sender: UIButton) {
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/ijybpprsmx0bgre/Seascape.mov")!
+        
+        playVideo(url: url)
+    }
+    
     
     private func playVideo(url: NSURL) {
         let playerController = AVPlayerViewController()
